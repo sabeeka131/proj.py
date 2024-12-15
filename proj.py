318ef59d03627544751d1fca6669d1fefdb05111
Aliza <59551300+sabeeka131@users.noreply.github.com>
2024-12-15 13:30:43 +0500

    Add files via upload

---
+++ proj.py
@@ -0,0 +1,12 @@
+char = input("Enter a character: ")
+alpha = {
+'a', 'b', 'c', 'd', 'e',
+'f', 'g', 'h', 'i', 'k',
+'m', 'o', 'q', 's', 'v',
+'x', 'y', 'z'
+}
+if char == alpha:
+    print("its is an alphabet")
+else:
+    print("its not an alphabet")4
+    
